d792e0760ef923fdf35c86a1b0c4a6d5e564262a
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-01-20 02:23:42 +0300

    7. ders tamamlandı

--- 1.Ders - Geliştirme ortamına giriş/IlkProjem/IlkProjem.ConsoleApp/Program.cs
+++ 1.Ders - Geliştirme ortamına giriş/IlkProjem/IlkProjem.ConsoleApp/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IlkProjem.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Çalışır");
-            Console.WriteLine("Senkron");
-            Console.WriteLine("Program");
-            Console.Read();
+            Console.Write("Sınav notunu giriniz: ");
+            int not = int.Parse(Console.ReadLine());
+
+            // Notun geçerli aralıkta olup olmadığını kontrol et
+            string mesaj = (not >= 0 && not <= 100)
+                ? (not >= 90 ? "Harf Notu: A"
+                    : not >= 80 ? "Harf Notu: B"
+                    : not >= 70 ? "Harf Notu: C"
+                    : not >= 60 ? "Harf Notu: D"
+                    : not >= 50 ? "Harf Notu: E"
+                    : "Harf Notu: F (Kaldı)")
+                : "Geçersiz bir not girdiniz. Lütfen 0 ile 100 arasında bir değer giriniz.";
+
+            Console.WriteLine(mesaj);
         }
     }
 }
---
+++ 7. Ders - Döngüler ve Debug/Kod/donguler_ve_debug/donguler_ve_debug/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donguler_ve_debug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayac = 0;
+            //while (sayac <= 10)
+            //{
+            //    Console.WriteLine(++sayac);
+            //}
+
+            //int[] sayilar = { 5, 2, 3, 4, 9, 6, 7, 8, 9, 78 };
+
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    Console.WriteLine(sayilar[i]);
+            //}
+
+            //int[] sayilar = { 5, 2, 3, 4, 9, 6, 7, 8, 9, 78 };
+            //foreach (var sayi in sayilar) //doğrudan dizi elemanları gezilebilir. ancak değpiştirilemez.
+            //{
+            //    Console.WriteLine(sayi);
+            //}
+
+            int sayi = 5;
+            Console.WriteLine("Sayı: " + sayi);
+            Console.ReadLine();
+        }
+    }
+}
