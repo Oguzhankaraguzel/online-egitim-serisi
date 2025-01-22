0e63b189f3c9a3cfa1d0d19d385845663c3405e0
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-01-23 00:23:15 +0300

    algoritma ve soru çözümü

---
+++ Algoritma Çalışmaları/FizzBuzz/FizzBuzz_Alg_2/FizzBuzz_Alg_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_Alg_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan bir sayı istenecek.
+            // sayı kadar eleman konsola yazdırılacak.
+            // ancak ekrana yazdılacak olan eleman 3'e bölünüyorsa Fizz, 5'e bölünüyorsa Buzz, 3 ve 5'e bölünüyorsa FizzBuzz yazılacak.
+            // 3 ve 5'e bölünmeyen sayılar normal yazılacak.
+            // sayı 1'den başlayacak ve kullanıcını verdiği sayı dahil olacak.
+
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            string[] array = new string[sayi];
+
+            #region alg
+            for (int i = 1; i <= sayi; i++)
+            {
+                if ((i % 3) == 0 && (i % 5) == 0)
+                    array[i - 1] = "FizzBuzz";
+                
+                else if ((i % 3) == 0)
+                    array[i - 1] = "Fizz";
+                
+                else if ((i % 5) == 0)
+                    array[i - 1] = "Buzz";
+                
+                else
+                    array[i - 1] = i.ToString();
+                
+            }
+            #endregion
+
+            foreach (string item in array)
+                Console.WriteLine(item);
+            
+        }
+    }
+}
---
+++ Soru çözümü/TekmiCiftmi/TekmiCiftmi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekmiCiftmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //int bitSayi = 0b_0100; // eğer bir sayının ilk biti 1 ise tek 0 ise çifttir.
+            // & işlemi ile sayının ilk bitini kontrol edebiliriz. & işlemi sayıları bit basında ve işlemine sokar.
+            // örnek olarak 5 & 4 = 101 & 100 = 100 = 4
+            // peki ben her sayıyı 1 ile and işlemine sokarsam ne olur?
+            // 5 & 1 = 101 & 001 = 001 = 1
+            // 4 & 1 = 100 & 001 = 000 = 0
+
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Girdiğiniz sayı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayı tektir.");
+            }
+
+            Console.WriteLine("Bit bazında kontrol: ");
+
+            if ((sayi & 1) == 0)
+            {
+                Console.WriteLine("Girdiğiniz sayı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayı tektir.");
+            }
+        }
+    }
+}
