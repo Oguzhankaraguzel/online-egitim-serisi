0734f82560ba6e371351fc390bfbffc9e8c07e78
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-01-27 23:32:51 +0300

    dokümanlar düzeltildi

--- 1.Ders - Geliştirme ortamına giriş/IlkProjem/IlkProjem.ConsoleApp/Program.cs
+++ 1.Ders - Geliştirme ortamına giriş/IlkProjem/IlkProjem.ConsoleApp/Program.cs
@@ -6,20 +6,13 @@ namespace IlkProjem.ConsoleApp
     {
         static void Main()
         {
-            Console.Write("Sınav notunu giriniz: ");
-            int not = int.Parse(Console.ReadLine());
-
-            // Notun geçerli aralıkta olup olmadığını kontrol et
-            string mesaj = (not >= 0 && not <= 100)
-                ? (not >= 90 ? "Harf Notu: A"
-                    : not >= 80 ? "Harf Notu: B"
-                    : not >= 70 ? "Harf Notu: C"
-                    : not >= 60 ? "Harf Notu: D"
-                    : not >= 50 ? "Harf Notu: E"
-                    : "Harf Notu: F (Kaldı)")
-                : "Geçersiz bir not girdiniz. Lütfen 0 ile 100 arasında bir değer giriniz.";
-
-            Console.WriteLine(mesaj);
+            SelamVer(); // 1. Metot çağrısı
+            SelamVer(); // 2. Metot çağrısı
+            SelamVer(); // 3. Metot çağrısı
+        }
+        public static void SelamVer() // Metot tanımlama
+        {
+            Console.WriteLine("Merhaba! Bu bir metot çağrısıdır."); // Metot içeriği
         }
     }
 }
