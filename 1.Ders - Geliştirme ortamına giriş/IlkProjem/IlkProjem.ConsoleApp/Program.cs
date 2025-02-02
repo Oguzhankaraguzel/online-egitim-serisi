1a9349e5aac2069d67811a103cbf91431279166a
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-02-02 21:00:36 +0300

    9. ve 10. ders dokümanları eklendi

--- 1.Ders - Geliştirme ortamına giriş/IlkProjem/IlkProjem.ConsoleApp/Program.cs
+++ 1.Ders - Geliştirme ortamına giriş/IlkProjem/IlkProjem.ConsoleApp/Program.cs
@@ -6,13 +6,27 @@ namespace IlkProjem.ConsoleApp
     {
         static void Main()
         {
-            SelamVer(); // 1. Metot çağrısı
-            SelamVer(); // 2. Metot çağrısı
-            SelamVer(); // 3. Metot çağrısı
+            Console.WriteLine("enter the first number:");
+            string firstınput = Console.ReadLine();
+
+            Console.WriteLine("enter the second number:");
+            string secondınput = Console.ReadLine();
+
+            int firstnumber = Convert.ToInt32(firstınput);
+            int secondnumber = Convert.ToInt32(secondınput);
+
+            int sum = AddTwoNumbers(firstnumber, secondnumber);
+
+            Console.WriteLine($"Answer: {sum}");
+            Console.ReadLine();
         }
-        public static void SelamVer() // Metot tanımlama
+        /// <summary>
+        /// prompts the user to enter two numbers, calculates their sum, and displays the result.
+        /// </summary>
+        static int AddTwoNumbers(int firstNum, int secondNum)
         {
-            Console.WriteLine("Merhaba! Bu bir metot çağrısıdır."); // Metot içeriği
+            int sum = firstNum + secondNum;
+            return sum;
         }
     }
 }
