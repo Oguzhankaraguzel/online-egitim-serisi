e522e68e97a5af41f1e898f6438739cb1bd78e2e
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-02-03 01:59:59 +0300

    9. ve 10. dersler işlendi

---
+++ 10. Ders - Metodlar 2. Bölüm/Kod/MetodlarIki/MetodlarIki/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodlarIki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter the first number: ");
+            //int ilkSayi = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Please enter the second number: ");
+            //int ikinciSayi = Convert.ToInt32(Console.ReadLine());
+            //int toplam = 0;
+            //toplam = SumTwoNumbers(ilkSayi, secondNumber: ikinciSayi);
+            //Console.WriteLine("The sum of the two numbers is: " + toplam);
+
+
+            //local değişken ve global değpişken farkları;
+
+            void Topla(int sayiBir, int sayiIki)
+            {
+                int toplam = sayiBir + sayiIki; //local değişken
+                Console.WriteLine($"metod içerisinde hesaplanan Girilen iki sayının toplamı: {toplam}");
+            }
+            int ilkSayi = 5;
+            int ikinciSayi = 10;
+            Topla(ilkSayi, ikinciSayi);
+            Console.WriteLine($"metod dışında hesaplanan Girilen iki sayının toplamı: {toplam}");
+
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// This method takes two numbers and returns the sum of them.
+        /// </summary>
+        /// <param name="firstNumber">first number</param>
+        /// <param name="secondNumber">second number, cannot negative</param>
+        /// <returns>firstNumber + secondNumber</returns>
+        static int SumTwoNumbers(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+
+    }
+}
---
+++ 9. Ders - Metodlar 1. Bölüm/Kod/metodlar/metodlar/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void PrintHelloNested()
+            {
+                string hello = "Hello World Nested!";
+                Console.WriteLine(hello);
+            }
+            PrintHelloNested();
+            PrintHello();
+            Console.ReadLine();
+        }
+        static void PrintHello() 
+        {
+            string hello = "Hello World!";
+            Console.WriteLine(hello);
+        }
+    }
+}
+// bu yorum satırlarını intelisense yazdı
+// Metodlar
+// Tekrar tekrar kullanılabilirliği sağlar
+// Kodun okunabilirliğini arttırır
+// Kodun düzenini sağlar
+// Tek bir yerden değişiklik yaparak tüm kodu etkileyebiliriz
+// Metodlar geriye değer döndürebilir
+// Geriye değer döndürmeyen metodlar void ile tanımlanır
+// Metodlar parametre alabilir
+// Parametreler metodun içinde kullanılabilir
+// Metodlar geriye değer döndürebilir
+// Geriye değer döndürmeyen metodlar void ile tanımlanır
+// Metodlar parametre alabilir
+// Parametreler metodun içinde kullanılabilir
+// Metodlar geriye değer döndürebilir
+// Geriye değer döndürmeyen metodlar void ile tanımlanır
+// Metodlar parametre alabilir
+// Parametreler metodun içinde kullanılabilir
+// Metodlar geriye değer döndürebilir
+// Geriye değer döndürmeyen metodlar void ile tanımlanır
+// Metodlar parametre alabilir
+// Parametreler metodun içinde kullanılabilir
+// Metodlar geriye değer döndürebilir
+// Geriye değer döndürmeyen metodlar void ile tanımlanır
+// Metodlar parametre alabilir
+// Parametreler metodun içinde kullanılabilir
+// Metodlar geriye değer döndürebilir
+// Geriye değer döndürmeyen metodlar void ile tanımlanır
+// Metodlar parametre alabilir
+// Parametreler metodun içinde kullanılabilir
+// Metodlar geriye değer döndürebilir
+// Geriye değer döndürmeyen metodlar void ile tanımlanır
