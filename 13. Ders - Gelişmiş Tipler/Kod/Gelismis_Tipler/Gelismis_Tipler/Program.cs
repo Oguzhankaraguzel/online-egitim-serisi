efc05ffcc5520175402b72acc7fb07f3a785eec0
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-04-26 16:34:30 +0300

    List ve Dictionary eklendi

---
+++ 13. Ders - Gelişmiş Tipler/Kod/Gelismis_Tipler/Gelismis_Tipler/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gelismis_Tipler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*vri tipim  değişken adım = değer   başlangıç değerleri ile oluşturalabilir*/
+            List<int> numbers = new List<int>() { 3, 5, 7, 9, 12};
+            numbers.Add(5); /*elemanı ekler*/
+            numbers.Add(3);
+            
+            //Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Capacity);
+            numbers.Remove(3); /* ilk bulduğu elemanı çıkartır. bulamazsa hate vermez*/
+            //Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Capacity);
+            numbers.RemoveAt(3);
+            //Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Capacity);
+            numbers.RemoveRange(0,3);
+            //Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Capacity);
+            numbers.Sort();
+            //Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Capacity);
+            numbers.Insert(0,2);
+            //Console.WriteLine(numbers.Count);
+            numbers.Add(3);
+            numbers.Add(3);
+            numbers.Add(3);
+            numbers.Add(3);
+            numbers.Add(3);
+            numbers.Add(3);
+            numbers.Add(3);
+            numbers.Add(3);
+            Console.WriteLine(numbers.Capacity);
+            Console.WriteLine();
+            if (numbers.Contains(1)) 
+            {
+                Console.WriteLine("listede 1 sayısı bulunmakta");
+            }
+            
+            foreach (int i in numbers) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Count; i++) 
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            _ = numbers.Count; /*listedeki eleman sayısını getirir.*/
+            
+        }
+    }
+}
---
+++ 13. Ders - Gelişmiş Tipler/Kod/Gelismis_Tipler_Dict/Gelismis_Tipler_Dict/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gelismis_Tipler_Dict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> yaslar = new Dictionary<string, int>()
+            {
+                {"Oğuzhan",30 },
+                {"Ahmet",30 },
+                {"Veli", 30 }
+            };
+            yaslar["Ayşe"] = 20; /*Anahtar yoksa oluşturulur ve değer atanır*/
+            yaslar["Ayşe"] = 20; /*Anahtar varsa değeri güncellenir*/
+            //yaslar.Add("Ayşe",25); /*Burada hata vermesinin sebebi. aynı anahtarı tekrar eklemeye çalışmasıdır*/
+            yaslar.Remove("Oğuzhan");
+            if (yaslar.ContainsKey("Oğuzhan"))
+            {
+                Console.WriteLine(yaslar["Oğuzhan"]);
+            }
+            foreach (var item in yaslar)
+            {
+                if (item.Value == 30)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+    }
+}
