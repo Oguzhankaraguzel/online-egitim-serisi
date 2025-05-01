ffa198168f1f15c6a4483ec9e98050e001449200
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-05-01 20:21:19 +0300

    Stack Konusu İşlendi

---
+++ 13. Ders - Gelişmiş Tipler/Kod/Gelismis_Tipler_Stack/Gelismis_Tipler_Stack/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Gelismis_Tipler_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack<string> books = new Stack<string>();
+            //books.Push("Kürk Mantolu Madonna");
+            //books.Push("Beyaz Diş");
+            //books.Push("Değirmen");
+
+            //string book = books.Peek();
+
+            //Console.WriteLine(book);
+
+            Stack<string> history = new Stack<string>();
+
+            void GoToUrl(string url)
+            {
+                history.Push(url);
+                Console.WriteLine($"Going To {url}");
+            }
+            void GoBack()
+            {
+                history.Pop();
+                Console.WriteLine($"Going To {history.Peek()}");
+            }
+            Console.WriteLine("Url Giriniz");
+            string Url = Console.ReadLine();
+            GoToUrl(Url);
+            while (true)
+            {
+                Console.WriteLine("Geri gitmek için \"g\" yazın. Url'e Gitmek için url yazın");
+                string command = Console.ReadLine();
+                if (command == "g")
+                {
+                    GoBack();
+                }
+                else
+                {
+                    GoToUrl(command);
+                }
+            }
+        }
+    }
+}
