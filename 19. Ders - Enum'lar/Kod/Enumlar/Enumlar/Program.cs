71488b6d13b17070925b85ea2618ee2126da6660
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-08-23 04:08:21 +0300

    Enum'lar işlendi

---
+++ 19. Ders - Enum'lar/Kod/Enumlar/Enumlar/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Enumlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OyunSeviyeleri mevcutSeviye = OyunSeviyeleri.Normal;
+            if (mevcutSeviye == OyunSeviyeleri.Kolay)
+            {
+                Console.WriteLine("Kolay mod");
+            }
+            else if (mevcutSeviye == OyunSeviyeleri.Normal)
+            {
+                Console.WriteLine("Normal mod");
+            }
+            else if (mevcutSeviye == OyunSeviyeleri.Zor)
+            {
+                Console.WriteLine("Zor mod");
+            }
+            else
+            {
+                Console.WriteLine("Hata");
+            }
+
+            Console.WriteLine("Mevcut seviye: " + (int)mevcutSeviye);
+
+            foreach (var item in Enum.GetValues(typeof(Aylar)))
+            {
+                Console.WriteLine((int)item + ". Ay : " + item.ToString());
+            }
+        }
+    }
+    public enum OyunSeviyeleri : byte
+    {
+        Kolay = 1,
+        Normal,
+        Zor
+    }
+    public enum  Aylar
+    {
+        Ocak = 1,
+        Subat,
+        Mart,
+        Nisan,
+        Mayis,
+        Haziran,
+        Temmuz,
+        Agustos,
+        Eylul,
+        Ekim,
+        Kasim,
+        Aralik
+    }
+}
