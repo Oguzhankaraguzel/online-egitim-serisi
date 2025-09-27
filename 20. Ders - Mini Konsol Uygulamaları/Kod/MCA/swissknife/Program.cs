6594107a71e36f418f5507ee69c37258771c9e3b
Oğuzhan KARAGÜZEL <Oguzhan.karaguzel@gmail.com>
2025-09-28 00:30:13 +0300

    Mini konsol uygulamaları projesi oluşturuldu

---
+++ 20. Ders - Mini Konsol Uygulamaları/Kod/MCA/swissknife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace swissknife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
